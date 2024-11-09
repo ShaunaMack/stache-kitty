2265ec4d4862ed799bed981c848a1646750135b1
Shauna Mack <shaunakatemack@gmail.com>
2024-11-09 15:35:26 +1000

    stache-kitty template starter

---
+++ README.md
@@ -0,0 +1,18 @@
+# Stache kitty
+
+There is a picture of a cute cat, and a moustache.
+
+Our task is to
+
+1. log the position of the mouse cursor
+2. implement an event listener that will put the moustache where the user clicks, so you can give the cute cat a moustache
+3. rotate the moustache so it looks good on the kitty
+4. add a caption under the image of the kitty
+5. add thought bubbles where we click
+6. add a control panel to enlarge, shrink and rotate the moustache
+
+Then we will add a caption
+
+### Some tips:
+
+- Two event properties that will help here are _pageX_ and _pageY_. These return coordinates of the mouse. Use console.log to see what is returned.
---
+++ script.js
@@ -0,0 +1,25 @@
+// locate the moustache in the DOM by the id
+const moustache = document.getElementById("");
+
+function addStache(event) {
+  const x = event.pageX; // Get the horizontal coordinate
+  const y = event.pageY; // Get the vertical coordinate
+
+  // log the position (x and y coordinates) of the stache
+
+  moustache.style.top = `${y - 50}px`;
+  moustache.style.left = `${x - 100}px`;
+}
+
+// Add an event listener to the correct DOM node here - using your addStache function, and firing on click.
+document.onclick = addStache;
+
+// now lets have even more fun...
+
+// lets add a caption when we click the cat picture
+document.getElementById("cat-pic").addEventListener("click", function () {
+  // define the caption variable using getElementById
+  // if there is no element with the id caption, make one with createElement (make sure you give it its id)
+  // use appendChild to add it to the bottom of the page
+  // set the caption to some words using innerHTML once caption is created
+});
